25a20ab1b35e2e10396ee5fbbd4b6e0507afb4ad
Maja Placek <104570804+mplacek22@users.noreply.github.com>
2024-06-11 16:18:16 +0200

    Update cognito.tf

--- terraform/cognito.tf
+++ terraform/cognito.tf
@@ -2,8 +2,9 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_cognito_user_pool" "test_pool" {
-  name = "AppPool"
+resource "aws_cognito_user_pool" "app_user_pool" {
+  name                = "AppPool"
+  deletion_protection = "INACTIVE"
 
   password_policy {
     minimum_length                  = 8
@@ -14,117 +15,12 @@ resource "aws_cognito_user_pool" "test_pool" {
     temporary_password_validity_days = 7
   }
 
-  deletion_protection = "INACTIVE"
-
   schema {
-    name                        = "profile"
-    attribute_data_type         = "String"
-    developer_only_attribute    = false
-    mutable                     = true
-    required                    = false
-    string_attribute_constraints {
-      min_length = 0
-      max_length = 2048
-    }
-  }
-
-  schema {
-    name                        = "address"
-    attribute_data_type         = "String"
-    developer_only_attribute    = false
-    mutable                     = true
-    required                    = false
-    string_attribute_constraints {
-      min_length = 0
-      max_length = 2048
-    }
-  }
-
-  schema {
-    name                        = "birthdate"
-    attribute_data_type         = "String"
-    developer_only_attribute    = false
-    mutable                     = true
-    required                    = false
-    string_attribute_constraints {
-      min_length = 10
-      max_length = 10
-    }
-  }
-
-  schema {
-    name                        = "gender"
-    attribute_data_type         = "String"
-    developer_only_attribute    = false
-    mutable                     = true
-    required                    = false
-    string_attribute_constraints {
-      min_length = 0
-      max_length = 2048
-    }
-  }
-
-  schema {
-    name                        = "pref_username"
-    attribute_data_type         = "String"
-    developer_only_attribute    = false
-    mutable                     = true
-    required                    = false
-    string_attribute_constraints {
-      min_length = 0
-      max_length = 2048
-    }
-  }
-
-  schema {
-    name                        = "updated_at"
-    attribute_data_type         = "Number"
-    developer_only_attribute    = false
-    mutable                     = true
-    required                    = false
-    number_attribute_constraints {
-      min_value = 0
-    }
-  }
-
-  schema {
-    name                        = "website"
-    attribute_data_type         = "String"
-    developer_only_attribute    = false
-    mutable                     = true
-    required                    = false
-    string_attribute_constraints {
-      min_length = 0
-      max_length = 2048
-    }
-  }
-
-  schema {
-    name                        = "picture"
-    attribute_data_type         = "String"
-    developer_only_attribute    = false
-    mutable                     = true
-    required                    = false
-    string_attribute_constraints {
-      min_length = 0
-      max_length = 2048
-    }
-  }
-
-  schema {
-    name                        = "identities"
-    attribute_data_type         = "String"
-    developer_only_attribute    = false
-    mutable                     = true
-    required                    = false
-  }
-
-  schema {
-    name                        = "sub"
-    attribute_data_type         = "String"
-    developer_only_attribute    = false
-    mutable                     = false
-    required                    = true
+    name                    = "sub"
+    attribute_data_type     = "String"
+    developer_only_attribute = false
+    mutable                 = false
+    required                = true
     string_attribute_constraints {
       min_length = 1
       max_length = 2048
@@ -132,11 +28,11 @@ resource "aws_cognito_user_pool" "test_pool" {
   }
 
   schema {
-    name                        = "phone_number"
-    attribute_data_type         = "String"
-    developer_only_attribute    = false
-    mutable                     = true
-    required                    = false
+    name                    = "email"
+    attribute_data_type     = "String"
+    developer_only_attribute = false
+    mutable                 = true
+    required                = true
     string_attribute_constraints {
       min_length = 0
       max_length = 2048
@@ -144,111 +40,19 @@ resource "aws_cognito_user_pool" "test_pool" {
   }
 
   schema {
-    name                        = "phone_verified"
-    attribute_data_type         = "Boolean"
-    developer_only_attribute    = false
-    mutable                     = true
-    required                    = false
+    name                    = "email_verified"
+    attribute_data_type     = "Boolean"
+    developer_only_attribute = false
+    mutable                 = true
+    required                = false
   }
 
   schema {
-    name                        = "zoneinfo"
-    attribute_data_type         = "String"
-    developer_only_attribute    = false
-    mutable                     = true
-    required                    = false
-    string_attribute_constraints {
-      min_length = 0
-      max_length = 2048
-    }
-  }
-
-  schema {
-    name                        = "locale"
-    attribute_data_type         = "String"
-    developer_only_attribute    = false
-    mutable                     = true
-    required                    = false
-    string_attribute_constraints {
-      min_length = 0
-      max_length = 2048
-    }
-  }
-
-  schema {
-    name                        = "email"
-    attribute_data_type         = "String"
-    developer_only_attribute    = false
-    mutable                     = true
-    required                    = true
-    string_attribute_constraints {
-      min_length = 0
-      max_length = 2048
-    }
-  }
-
-  schema {
-    name                        = "email_verified"
-    attribute_data_type         = "Boolean"
-    developer_only_attribute    = false
-    mutable                     = true
-    required                    = false
-  }
-
-  schema {
-    name                        = "given_name"
-    attribute_data_type         = "String"
-    developer_only_attribute    = false
-    mutable                     = true
-    required                    = false
-    string_attribute_constraints {
-      min_length = 0
-      max_length = 2048
-    }
-  }
-
-  schema {
-    name                        = "family_name"
-    attribute_data_type         = "String"
-    developer_only_attribute    = false
-    mutable                     = true
-    required                    = false
-    string_attribute_constraints {
-      min_length = 0
-      max_length = 2048
-    }
-  }
-
-  schema {
-    name                        = "middle_name"
-    attribute_data_type         = "String"
-    developer_only_attribute    = false
-    mutable                     = true
-    required                    = false
-    string_attribute_constraints {
-      min_length = 0
-      max_length = 2048
-    }
-  }
-
-  schema {
-    name                        = "name"
-    attribute_data_type         = "String"
-    developer_only_attribute    = false
-    mutable                     = true
-    required                    = false
-    string_attribute_constraints {
-      min_length = 0
-      max_length = 2048
-    }
-  }
-
-  schema {
-    name                        = "nickname"
-    attribute_data_type         = "String"
-    developer_only_attribute    = false
-    mutable                     = true
-    required                    = false
+    name                    = "preferred_username"
+    attribute_data_type     = "String"
+    developer_only_attribute = false
+    mutable                 = true
+    required                = true
     string_attribute_constraints {
       min_length = 0
       max_length = 2048
@@ -293,59 +97,29 @@ resource "aws_cognito_user_pool" "test_pool" {
 }
 
 resource "aws_cognito_user_pool_client" "public_client" {
-  name                = "public client"
-  user_pool_id        = aws_cognito_user_pool.test_pool.id
-  generate_secret     = false
+  name            = "public client"
+  user_pool_id    = aws_cognito_user_pool.app_user_pool.id
+  generate_secret = false
 
-  refresh_token_validity  = 30
-  access_token_validity   = 60
-  id_token_validity       = 60
+  refresh_token_validity = 30
+  access_token_validity  = 60
+  id_token_validity      = 60
+
   token_validity_units {
-    access_token = "minutes"
-    id_token     = "minutes"
+    access_token  = "minutes"
+    id_token      = "minutes"
     refresh_token = "days"
   }
 
-  read_attributes  = [
-    "address",
-    "birthdate",
+  read_attributes = [
     "email",
     "email_verified",
-    "family_name",
-    "gender",
-    "given_name",
-    "locale",
-    "middle_name",
-    "name",
-    "nickname",
-    "phone_number",
-    "phone_number_verified",
-    "picture",
-    "preferred_username",
-    "profile",
-    "updated_at",
-    "website",
-    "zoneinfo"
+    "preferred_username"
   ]
 
   write_attributes = [
-    "address",
-    "birthdate",
     "email",
-    "family_name",
-    "gender",
-    "given_name",
-    "locale",
-    "middle_name",
-    "name",
-    "nickname",
-    "phone_number",
-    "picture",
-    "preferred_username",
-    "profile",
-    "updated_at",
-    "website",
-    "zoneinfo"
+    "preferred_username"
   ]
 
   explicit_auth_flows = [
@@ -357,6 +131,6 @@ resource "aws_cognito_user_pool_client" "public_client" {
   allowed_oauth_flows_user_pool_client = false
 
   prevent_user_existence_errors = "ENABLED"
-  enable_token_revocation = true
-  auth_session_validity = 3
+  enable_token_revocation       = true
+  auth_session_validity         = 3
 }
